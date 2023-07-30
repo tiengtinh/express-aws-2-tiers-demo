4ae483b011a7a4479e654017e54426eb4e2ab2d2
Tinh Ngo <tiengtinh@gmail.com>
2023-07-30 20:45:42 +0700

    feawt: cloudfront image

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`<h1>Tinh</h1>
+  <p>
+    CloudFront Image with S3 Origin: https://dget5eqckjiws.cloudfront.net/f132676070524afd8e20945648c37314.jpg
+  </p>
+  <img src="https://dget5eqckjiws.cloudfront.net/f132676070524afd8e20945648c37314.jpg" />
+  `)
 })
 
 app.listen(port, () => {
